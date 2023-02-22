8798ba830250f9523ddfa707ec64c6f08398a18d
alienix2 <matteomoni@gmail.com>
2023-02-22 09:26:04 +0100

    Refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,6 +20,22 @@ class AlienInvasion:
         #Disegno la navetta
         self.ship = Ship(self)
         
+    def check_keydown_events(self, event):
+        """Risponde se un pulsante è premuto"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+            
+    def check_keyup_events(self, event):
+        """Risponde se un pulsante è rilasciato"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        
     def check_events(self):
         """"Controlla se sono avvenuti eventi e si comporta di conseguenza"""
         
@@ -27,15 +43,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
+                    self.check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+                    self.check_keyup_events(event)
                 
                 
     def update_screen(self):
